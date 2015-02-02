ea47a0bb4c7a070901a252c28376a734608dc87f
Glarmo <GuillermVillalobos@hotmail.com>
2015-02-02 22:04:08 +0000

    initial

---
+++ Asteroids/Assets/_scripts/asteroids.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class asteroids : MonoBehaviour 
+{
+	public float rotateSpeed;
+	public GameObject childAsteroid;
+	
+	void Update ()
+	{
+		transform.Rotate(0,rotateSpeed,0);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Laser")
+		{
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+
+		}
+	}
+}
---
+++ Asteroids/Assets/_scripts/playerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour 
+{
+	Vector3 dir = Vector3.zero;
+	int rotationSpeed = 20;
+	int speed = 2;
+	
+	void Update () 
+	{
+		// moves the ship
+		transform.Translate(Input.acceleration.x * speed, 0, Input.acceleration.y * speed, Space.World);
+
+		// rotates the ship
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+
+		if (dir != Vector3.zero) {
+			transform.rotation = Quaternion.Slerp(
+				transform.rotation,
+				Quaternion.LookRotation(dir),
+				Time.deltaTime * rotationSpeed
+				);
+		}
+		
+		transform.Rotate(Vector3.forward*Input.acceleration.y*5.0f);
+
+		// limits the ship to sceen space
+		Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
+		pos.x = Mathf.Clamp01 (pos.x);
+		pos.y = Mathf.Clamp01 (pos.y);
+		transform.position = Camera.main.ViewportToWorldPoint(pos);
+		Vector3 yPos = transform.position;
+		yPos.y = 0;
+		transform.position = yPos;
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Hazard")
+		{
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
