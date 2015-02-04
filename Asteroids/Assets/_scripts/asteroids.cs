82cbb7f1239f6ad0a5b26a7337ac8caefbbc0e18
Glarmo <guillermo@ewertcs08.doverbroecks.com>
2015-02-04 14:26:54 +0000

    function of spawing asteroids

--- Asteroids/Assets/_scripts/asteroids.cs
+++ Asteroids/Assets/_scripts/asteroids.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class asteroids : MonoBehaviour 
 {
-	public float rotateSpeed;
+	private float rotateSpeed;
 	public GameObject childAsteroid;
-	
+
+	void Start ()
+	{
+		rotateSpeed = Random.Range (2, 5);
+	}
 	void Update ()
 	{
 		transform.Rotate(0,rotateSpeed,0);
+		Vector3 Pos = transform.position;
+		Pos.y = 0;
+		transform.position = Pos;
 	}
 	void OnTriggerEnter(Collider other) 
 	{
--- Asteroids/Assets/_scripts/gameController.cs
+++ Asteroids/Assets/_scripts/gameController.cs
@@ -5,17 +5,51 @@ public class gameController : MonoBehaviour
 {
 	public GameObject hazard;
 	public Vector3 topSpawnValues;
+	public Vector3 rightSpawnValues;
+	public Vector3 leftSpawnValues;
 	public Vector3 bottomSpawnValues;
 	
 	void Start ()
 	{
-		spawnAsteroids (topSpawnValues, bottomSpawnValues);
+		spawnEnemyStraight (hazard);
 	}
 
-	void spawnAsteroids (Vector3 spawnValues, Vector3 direction)
+	void spawnEnemyStraight (GameObject enemy)
 	{
-		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		GameObject enemyInstance;
+		int whereSpawn = Random.Range (1, 4);
+
+		if (whereSpawn == 1)		// spawns top
+		{
+			Vector3 spawnPosition = new Vector3 (Random.Range (-topSpawnValues.x, topSpawnValues.x), topSpawnValues.y, topSpawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			enemyInstance = Instantiate (enemy, spawnPosition, spawnRotation) as GameObject;
+			enemyInstance.rigidbody.AddForce(Vector3.back * 1000);
+
+		}
+		else if (whereSpawn == 2)		// spawns right
+		{
+			Vector3 spawnPosition = new Vector3 (rightSpawnValues.x, rightSpawnValues.y, Random.Range (-rightSpawnValues.z, rightSpawnValues.z));
+			Quaternion spawnRotation = Quaternion.identity;
+			enemyInstance = Instantiate (enemy, spawnPosition, spawnRotation) as GameObject;
+			enemyInstance.rigidbody.AddForce(Vector3.left * 1000);
+		}
+		else if (whereSpawn == 3)		// spawns left
+		{
+			Vector3 spawnPosition = new Vector3 (leftSpawnValues.x, leftSpawnValues.y, Random.Range (-leftSpawnValues.z, leftSpawnValues.z));
+			Quaternion spawnRotation = Quaternion.identity;
+			enemyInstance = Instantiate (enemy, spawnPosition, spawnRotation) as GameObject;
+			enemyInstance.rigidbody.AddForce(Vector3.right * 1000);
+		}
+		else if (whereSpawn == 4)		// spawns bottom
+		{
+			Vector3 spawnPosition = new Vector3 (Random.Range (-bottomSpawnValues.x, bottomSpawnValues.x), bottomSpawnValues.y, bottomSpawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			enemyInstance = Instantiate (enemy, spawnPosition, spawnRotation) as GameObject;
+			enemyInstance.rigidbody.AddForce(Vector3.forward * 1000);
+		}
+
+
+
 	}
 }
