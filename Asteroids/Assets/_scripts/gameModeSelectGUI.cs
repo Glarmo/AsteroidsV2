6be52e1b883774317b40688f8a670d435c8775e4
Glarmo <GuillermVillalobos@hotmail.com>
2015-02-12 23:48:24 +0000

    a way to access the new gamemode

--- Asteroids/Assets/_scripts/gameModeSelectGUI.cs
+++ Asteroids/Assets/_scripts/gameModeSelectGUI.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class gameModeSelectGUI : MonoBehaviour 
 {
-	public GUISkin colouredRocks;
+	public GUISkin shootColouredRocks;
+	public GUISkin smashColouredRocks;
 
 	void OnGUI ()
 	{
-		GUI.skin = colouredRocks;
+		GUI.skin = shootColouredRocks;
 		if (GUI.Button (new Rect (Screen.width / 2 - 250, Screen.height / 2 - 125, 500, 250), ""))
 		{
 			Application.LoadLevel ("level1");
+		}
+		GUI.skin = smashColouredRocks;
+		if (GUI.Button (new Rect (Screen.width / 2 - 250, Screen.height / 2 + 125, 500, 250), ""))
+		{
+			Application.LoadLevel ("level2");
 		}
 	}
 }
